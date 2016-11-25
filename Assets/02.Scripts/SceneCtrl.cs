a601e60aa4f4ec2ca8de9137b6f91ea3676d559d
JPark_Laptop <junnypjb@gmail.com>
2016-11-26 02:55:57 +0900

    Edited

--- Assets/02.Scripts/SceneCtrl.cs
+++ Assets/02.Scripts/SceneCtrl.cs
@@ -1,13 +1,16 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.SceneManagement;
 
 public class SceneCtrl : MonoBehaviour {
 
 	void Start () {
-	
 	}
+
+    void update() {
+            Debug.Log("d");
+        
+            SceneManager.LoadScene("Menu", LoadSceneMode.Additive);
+    }
 	
-	void Update () {
-	
-	}
 }
