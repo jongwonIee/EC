8f88542c6d752de6f61dd7368a9388e456ed456a
lee <ljw3728@gmail.com>
2016-12-11 08:09:36 +0900

    발사 버튼 애니메이션 사운드 추가, 폴더정리

---
+++ Assets/02.Scripts/GunFire.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class GunFire : MonoBehaviour {
+    public AudioClip fireSfx;
+    private Animation gunanim;
+    private AudioSource _audio;
+
+    void Start() 
+    {   
+        _audio = GetComponent<AudioSource>();
+        gunanim = GetComponent<Animation>();
+    }
+
+	void Update () {
+        if (CnControls.CnInputManager.GetButtonDown("Fire1"))
+        {
+            Fire();
+        }
+	}
+
+    void Fire()
+    {
+        _audio.PlayOneShot(fireSfx, 1.0f);
+        gunanim.Play("GunShot");
+    }
+}
