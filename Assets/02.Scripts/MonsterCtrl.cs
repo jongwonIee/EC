9fc758d30813c2147349dd89db7e7ba7e6da227d
lee <ljw3728@gmail.com>
2016-12-12 06:23:18 +0900

    몬스터 체력구현

---
+++ Assets/02.Scripts/MonsterCtrl.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MonsterCtrl : MonoBehaviour {
+    
+    private float hp = 20.0f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnDamage(object[] _params)
+    {
+        Debug.Log("Hit!");
+        hp -= (int) _params[1];
+        if (hp <= 0.0f)
+        {
+//            MonsterDie();
+            Debug.Log("Dead!");
+        }      
+    }
+}
