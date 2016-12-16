0e558dc3012baa157436d99837108ca5f737c30f
JPark_Laptop <junnypjb@gmail.com>
2016-12-17 01:53:00 +0900

    GunFire/RayCast bug Fixed

--- Assets/02.Scripts/InGameUI.cs
+++ Assets/02.Scripts/InGameUI.cs
@@ -35,8 +35,7 @@ public class InGameUI : MonoBehaviour {
     void Update (){
         RaycastHit hit;
 
-        if (Physics.Raycast (firePos.position, firePos.TransformDirection(Vector3.forward), out hit))
-        {
+        if (Physics.Raycast(firePos.position, firePos.forward, out hit)) {
             TargetDistance = hit.distance;
             if (TargetDistance < Range && hit.collider.tag == "MONSTER")
             {
