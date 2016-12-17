8a5099b16002dd207c1091e7e85c0a3ae4dec35a
JPark_Laptop <junnypjb@gmail.com>
2016-12-17 10:54:18 +0900

    Here

--- Assets/Standard Assets/Characters/FirstPersonCharacter/Scripts/FirstPersonController.cs
+++ Assets/Standard Assets/Characters/FirstPersonCharacter/Scripts/FirstPersonController.cs
@@ -204,8 +204,8 @@ namespace UnityStandardAssets.Characters.FirstPerson
         private void GetInput(out float speed)
         {
             // Read input
-            float horizontal = CrossPlatformInputManager.GetAxis("Horizontal");
-            float vertical = CrossPlatformInputManager.GetAxis("Vertical");
+            float horizontal = CnControls.CnInputManager.GetAxis("Horizontal");
+            float vertical = CnControls.CnInputManager.GetAxis("Vertical");
 
             bool waswalking = m_IsWalking;
 
