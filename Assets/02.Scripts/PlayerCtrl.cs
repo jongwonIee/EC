99fe809b6a1e41e8fc4d2ddc9e83a3a6e55f6fc6
lee <ljw3728@gmail.com>
2016-12-21 11:37:54 +0900

    Added Facebook api

--- Assets/02.Scripts/PlayerCtrl.cs
+++ Assets/02.Scripts/PlayerCtrl.cs
@@ -2,15 +2,6 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerCtrl : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+public class PlayerCtrl : MonoBehaviour {
 }
