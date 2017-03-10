c6c7f9b5d070bf0f3a07f87d1c7ab93d44acc946
jongwonIee <ljw3728@gmail.com>
2017-03-10 22:35:19 +0900

    GameManager 생성

--- Assets/02.Scripts/GameMgr.cs
+++ Assets/02.Scripts/GameMgr.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GameManager : MonoBehaviour {
+public class GameMgr : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
