21cf3b04bf2f33086d13eebcc2145f3e2df270cd
DESKTOP-CF94FFI\junny <junnypjb@gmail.com>
2017-05-10 00:41:52 +0900

    merge commit

---
+++ Assets/02.Scripts/GameMgr.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameMgr : MonoBehaviour {
+    public Transform[] points;
+    public GameObject monsterPrefab;
+    public float createTime = 2.0f;
+    public int maxMonster = 10;
+    public bool isGameOver = false;
+    public static GameMgr instance = null;
+
+    void Awake ()
+    {
+        instance = this;
+    }
+
+	// Use this for initialization
+	void Start () {
+        points = GameObject.Find("MonsterSpawnPoint").GetComponentsInChildren<Transform>();
+
+        if (points.Length > 0)
+        {
+            StartCoroutine(this.CreateMonster());
+        }
+		
+	}
+
+    IEnumerator CreateMonster()
+    {
+        while (!isGameOver)
+        {
+            int monsterCount = (int)GameObject.FindGameObjectsWithTag("MONSTER").Length;
+
+            if (monsterCount < maxMonster)
+            {
+                yield return new WaitForSeconds(createTime);
+
+                int idx = Random.Range(1, points.Length);
+                Instantiate(monsterPrefab, points[idx].position, points[idx].rotation);
+            }
+            else
+            {
+                yield return null;
+            }
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown("escape"))
+            Application.Quit();
+	}
+}
--- Assets/02.Scripts/PlayerCtrl.cs
+++ Assets/02.Scripts/PlayerCtrl.cs
@@ -1,10 +1,48 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 
 public class PlayerCtrl : MonoBehaviour {
-    public void Awake() {
-        DontDestroyOnLoad(this.gameObject);
+    public int hp = 100;
+    private int initHp;
+    public Image imgHpbar;
+    private Transform tr;
+  
+    void Start(){
+        initHp = hp;
+
+        tr = GetComponent<Transform>();
+
+    }
+    void Update(){
+    }
+    void OnTriggerEnter(Collider coll)
+    {
+        if (coll.gameObject.tag == "PUNCH")
+        {
+            hp -= 10;
+            Debug.Log(hp);
+        }
+
+        if (hp <= 0)
+        {
+            PlayerDie();
+        }
+    }
+
+    void PlayerDie()
+    {
+        Debug.Log("Player Die!!");
+
+        OnPlayerDie();
+
+        GameMgr.instance.isGameOver = true;
+    }
+
+    void OnPlayerDie()
+    {
+        Debug.Log("Game End!!");
     }
 }
